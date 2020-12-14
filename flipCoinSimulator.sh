48707116ed132ee80a1534841da28253d8f04d7e
yatiP <yati.patil17@gmail.com>
2020-12-14 13:24:33 +0530

    Simulation to check winning Difference between H/T

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
 echo "Welcome to Flip Coin Simulation"
+
+H=0
+T=1
+HC=0
+TC=0
+
+for (( i=1; i>0; i++ ))
+do
+	coinFlip=$((RANDOM%2));
+	if [ $coinFlip -eq 1 ];
+	then
+		HC=$(( $HC + 1 ))
+		if [ $HC -eq 21 ]
+		then
+			res=$(( HC-TC ))
+			echo "Head wins with $res difference";
+			exit
+		fi
+	fi
+
+	if [ $coinFlip -eq 0 ];
+	then
+		TC=$(( $TC + 1 ))
+		if [ $TC -eq 21 ]
+                then
+                        res=$(( TC-HC ))
+                        echo "Tail wins with $res difference";
+			exit
+                fi
+
+	fi
+done
+
+echo "Number of times Head: " $HC
+echo "Number of times Tail: " $TC
